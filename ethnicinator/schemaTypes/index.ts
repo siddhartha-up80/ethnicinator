5475f14c77cbd0b84318412f40e005dd74617b10
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-02-17 23:04:33 +0530

    Navbar done

---
+++ ethnicinator/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import category from '../schemas/category'
+import heroImages from '../schemas/heroImages'
+import product from '../schemas/product'
+
+export const schemaTypes = [product, category, heroImages]
